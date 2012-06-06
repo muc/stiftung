780c2d82445f6d043f3a7b607db2a84dc0d199a7
Marc Hitscherich <marc.hitscherich@erdfisch.de>
2012-06-06 14:21:52 +0200

    breadcrumb fix

--- sites/all/themes/buergerstiftung/template.php
+++ sites/all/themes/buergerstiftung/template.php
@@ -11,3 +11,27 @@
  * for more information on this topic.
  */
 
+/**
+ * Returns HTML for a breadcrumb trail.
+ *
+ * @param $variables
+ *   An associative array containing:
+ *   - breadcrumb: An array containing the breadcrumb links.
+ */
+function buergerstiftung_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (!empty($breadcrumb)) {
+    $breadcrumb[0] = l(t('Startseite'), '<front>');
+    $title = drupal_get_title();
+    if (!empty($title)) {
+      $breadcrumb[] = '<span class="breadcrumb-current">'. $title .'</span>';
+    }
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . '</div>';
+    return $output;
+  }
+}
