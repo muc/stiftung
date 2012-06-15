559bdccdb1191e72536764022ec95cc4acf71c69
Paul R. <paul@paul-rohrbeck.de>
2012-06-15 22:27:03 +0200

    404 seite getextet + bissle gestylt

--- sites/all/themes/buergerstiftung/templates/page--404.tpl.php
+++ sites/all/themes/buergerstiftung/templates/page--404.tpl.php
@@ -3,12 +3,29 @@
     <?php print render($page['header']); ?>
   <?php endif; ?>
   <section id="section-content">
-    <div class="container-12">
-      <div class="grid-10 prefix-2">
-        <p></p>
-        <h1>Seite nicht gefunden :(</h1>
-      </div>
-    </div>
+	  
+		<div class="container-12 clearfix">
+		  <div class="grid-12 node">
+		  
+			<h1 class="heading-404">Seite nicht gefunden (Error 404)</h1>
+			<p>Es scheint als wäre die Seite, die Sie zu erreichen versucht haben, nicht verfügbar. Das kann verschiedene Gründe haben:</p>
+			
+			<ul>
+				<li>Die Seite wurde <strong>gelöscht</strong></li>
+				<li>Die Seite wurde <strong>umbenannt</strong></li>
+				<li>Der Link auf die Seite ist falsch oder Sie haben sich <strong>vertippt</strong></li>
+			</ul>
+			
+			<p>Sie können nun folgendes tun:</p>
+			<ul>
+				<li>Überprüfen Sie die Adresse auf eventuelle Tippfehler</li>
+				<li>Nutzen Sie unsere <strong>Suchfunktion</strong> (siehe oben Rechts)</li>
+				<li>Gehen Sie auf unsere <strong>Startseite</strong> (Klick auf das Logo oben links) und starten Sie von Neuem</li>
+			</ul>
+					
+		  </div>
+		</div>
+	
   </section>
   <?php if (isset($page['footer'])) : ?>
     <?php print render($page['footer']); ?>
